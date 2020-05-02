b7c1c3572b8aba3bc204d007581cab533be635f8
David Álvarez Pons <david_alvarezpons@hotmail.com>
2020-05-02 17:09:10 +0200

    Add slim4 to php server

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ help:		## Show this help.
 	@fgrep -h "##" $(MAKEFILE_LIST) | fgrep -v fgrep | sed -e 's/\\$$//' | sed -e 's/##//'
 .PHONY: run
 run:		## Start the services
-	@docker-compose up
+	@docker-compose run php-http composer install && docker-compose up
 .PHONY: build
 build:		## Build the containers
 	@docker-compose build
@@ -14,3 +14,4 @@ test:		## Execute test
 
 .PHONY: clean
 clean:		## Clean all the data created
+	@rm -rf php/vendor
--- php/server/index.php
+++ php/server/index.php
@@ -1,3 +1,20 @@
 <?php
 
-echo "(php-http) Hello World\n";
+include __DIR__ . '/../vendor/autoload.php';
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+$app->addRoutingMiddleware();
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+$app->get(
+    '/',
+    function (Request $request, Response $response) {
+        $response->getBody()->write("(php-http) Hello World\n");
+        return $response;
+    }
+);
+$app->run();
