aa6012397d6ddf73f55ac17c39d6d8ad467d9b1c
David Álvarez Pons <david_alvarezpons@hotmail.com>
2020-05-03 11:50:12 +0200

    Implement FibFac in php-http service

--- php/server/index.php
+++ php/server/index.php
@@ -6,6 +6,23 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
+function fibonacci($x)
+{
+    if ($x < 2) {
+        return 1;
+    }
+    return fibonacci($x - 1) + fibonacci($x - 2);
+}
+
+function factorial($x)
+{
+    $fact = 1;
+    foreach (range(1, $x + 1) as $i) {
+        $fact *= $i;
+    }
+    return $fact;
+}
+
 $app = AppFactory::create();
 $app->addRoutingMiddleware();
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
@@ -14,6 +31,21 @@ $app->get(
     '/',
     function (Request $request, Response $response) {
         $response->getBody()->write("(php-http) Hello World\n");
+        return $response;
+    }
+);
+$app->get(
+    '/fibfac',
+    function (Request $request, Response $response) {
+        $params = $request->getQueryParams();
+        $a = $params['a'] ?? null;
+        $b = $params['b'] ?? null;
+        if (!is_null($a) && !is_null($b)) {
+            $fibFac = fibonacci($a) + factorial($b);
+            $response->getBody()->write("$fibFac");
+        } else {
+            $response->withStatus(400);
+        }
         return $response;
     }
 );
