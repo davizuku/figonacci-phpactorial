ba51e8995f88cd77db9d67b977d2745ffd36c9b0
David Álvarez Pons <david_alvarezpons@hotmail.com>
2020-05-03 20:12:41 +0200

    Add local php client

---
+++ php/client/LocalClient.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace FigonacciPhpactorial\Client;
+
+use FigonacciPhpactorial\Calculators\Factorial;
+use FigonacciPhpactorial\Calculators\Fibonacci;
+
+class LocalClient
+{
+    protected $mod;
+
+    public function __construct(int $mod)
+    {
+        $this->mod = $mod;
+    }
+
+    public function helloWorld(): string
+    {
+        return "Hello World";
+    }
+
+    public function fibFac(int $x): float
+    {
+        $fib = new Fibonacci();
+        $fac = new Factorial();
+        return $fib($x, $this->mod) + $fac($x, $this->mod);
+    }
+}
