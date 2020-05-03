fca0acefd522be7833f0ed22f7f09f92e72c47dc
David Álvarez Pons <david_alvarezpons@hotmail.com>
2020-05-03 12:08:18 +0200

    Implement FibFac in go-http language

--- golang/cmd/http.go
+++ golang/cmd/http.go
@@ -7,10 +7,26 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
 	"time"
 
 	"github.com/gorilla/mux"
 )
+
+func fibonacci(x int) int {
+	if x < 2 {
+		return 1
+	}
+	return fibonacci(x-1) + fibonacci(x-2)
+}
+
+func factorial(x int) int {
+	fact := 1
+	for i := 1; i <= x; i++ {
+		fact *= i
+	}
+	return fact
+}
 
 func main() {
 	logger := log.New(os.Stdout, "go-http", log.LstdFlags)
@@ -18,6 +34,28 @@ func main() {
 	router.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
 		logger.Println("Handling GET on route '/'")
 		fmt.Fprintf(res, "(go-http) Hello World\n")
+	})
+	router.HandleFunc("/fibfac", func(res http.ResponseWriter, req *http.Request) {
+		logger.Println("Handling GET on route '/fibfac'")
+		paramsA, ok := req.URL.Query()["a"]
+		if !ok || len(paramsA) != 1 {
+			http.Error(res, "Invalid or missing 'a' query param", http.StatusBadRequest)
+		}
+		a, err := strconv.Atoi(paramsA[0])
+		if err != nil {
+			http.Error(res, "Invalid 'a' query param", http.StatusBadRequest)
+		}
+		paramsB, ok := req.URL.Query()["b"]
+		if !ok || len(paramsB) != 1 {
+			http.Error(res, "Invalid or missing 'b' query param", http.StatusBadRequest)
+		}
+		b, err := strconv.Atoi(paramsB[0])
+		if err != nil {
+			http.Error(res, "Invalid 'b' query param", http.StatusBadRequest)
+		}
+		fib := fibonacci(a)
+		fac := factorial(b)
+		fmt.Fprintf(res, "%d", fib+fac)
 	})
 	port := "80"
 	server := &http.Server{
