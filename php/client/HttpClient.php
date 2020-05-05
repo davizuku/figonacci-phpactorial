775bc3e1d1716bf0641a0b128dc51fb941f2dac2
David Álvarez Pons <david_alvarezpons@hotmail.com>
2020-05-05 20:01:22 +0200

    Add HttpClient

---
+++ php/client/HttpClient.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace FigonacciPhpactorial\Client;
+
+use GuzzleHttp\Client as GuzzleClient;
+
+class HttpClient
+{
+    /** @var GuzzleClient */
+    protected $guzzle;
+
+    public function __construct(string $url)
+    {
+        $this->guzzle = new GuzzleClient(['base_uri' => $url]);
+    }
+
+    public function helloWorld(): string
+    {
+        $response = $this->guzzle->get('/');
+        return (string) $response->getBody();
+    }
+
+    public function fibFac(int $x): float
+    {
+        $response = $this->guzzle->get('/fibfac', ['query' => ['a' => $x]]);
+        return (float) $response->getBody();
+    }
+}
