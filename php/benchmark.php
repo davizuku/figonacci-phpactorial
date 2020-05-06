0864e866c7961346d40ea2bbe3e2a951cb551cf3
David Álvarez Pons <david_alvarezpons@hotmail.com>
2020-05-06 19:40:39 +0200

    Add grpc client

--- php/benchmark.php
+++ php/benchmark.php
@@ -1,5 +1,6 @@
 <?php
 
+use FigonacciPhpactorial\Client\GrpcClient;
 use FigonacciPhpactorial\Client\HttpClient;
 use FigonacciPhpactorial\Client\LocalClient;
 
@@ -9,13 +10,16 @@ $mod = getenv('FIBFAC_MOD');
 $local = new LocalClient($mod);
 $httpPhp = new HttpClient('http://nginx-http');
 $httpGo = new HttpClient('http://go-http');
+$grpcGo = new GrpcClient('go-grpc:80');
 
 echo "Local HelloWorld: '{$local->helloWorld()}'\n";
 echo "HTTP PHP HelloWorld: '{$httpPhp->helloWorld()}'\n";
 echo "HTTP GO HelloWorld: '{$httpGo->helloWorld()}'\n";
+echo "GRPC GO HelloWorld: '{$grpcGo->helloWorld()}'\n";
 echo "\n-------------\n\n";
 
 echo "Local FibFac: '{$local->fibFac(10)}'\n";
 echo "HTTP PHP FibFac: '{$httpPhp->fibFac(10)}'\n";
 echo "HTTP GO FibFac: '{$httpGo->fibFac(10)}'\n";
+echo "GRPC GO FibFac: '{$grpcGo->fibFac(10)}'\n";
 echo "\n-------------\n\n";
---
+++ php/client/GrpcClient.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace FigonacciPhpactorial\Client;
+
+use Exception;
+use GRPC\Benchmark\BenchmarkClient;
+use GRPC\Benchmark\FibFacRequest;
+use GRPC\Hello\HelloWorldClient as HelloWorldClient;
+use GRPC\Hello\PBEmpty;
+
+class GrpcClient implements ClientInterface
+{
+    /** @var HelloWorldClient */
+    protected $helloClient;
+
+    /** @var BenchmarkClient */
+    protected $fibFacClient;
+
+    public function __construct(string $url)
+    {
+        $this->helloClient = new HelloWorldClient($url, [
+            'credentials' => \Grpc\ChannelCredentials::createInsecure(),
+        ]);
+        $this->fibFacClient = new BenchmarkClient($url, [
+            'credentials' => \Grpc\ChannelCredentials::createInsecure(),
+        ]);
+    }
+
+    public function helloWorld(): string
+    {
+        $request = new PBEmpty();
+        /** @var \GRPC\Hello\HelloResponse $response */
+        list($response, $status) = $this->helloClient->Speak($request)->wait();
+        if ($status->code !== 0) {
+            throw new Exception($status->details, $status->code);
+        }
+        return $response->getMsg();
+    }
+
+    public function fibFac(int $x): float
+    {
+        $request = new FibFacRequest();
+        $request->setA($x);
+        /** @var \GRPC\Benchmark\FibFacResponse $response */
+        list($response, $status) = $this->fibFacClient->FibFac($request)->wait();
+        if ($status->code !== 0) {
+            throw new Exception($status->details, $status->code);
+        }
+        return $response->getValue();
+    }
+}
