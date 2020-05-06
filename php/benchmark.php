a956a874decb0d6bd0c7a83c15a354651057aeef
David Álvarez Pons <david_alvarezpons@hotmail.com>
2020-05-06 19:50:42 +0200

    Refactor benchmark

--- php/benchmark.php
+++ php/benchmark.php
@@ -1,23 +1,28 @@
 <?php
 
+use FigonacciPhpactorial\Client\ClientInterface;
 use FigonacciPhpactorial\Client\GrpcClient;
 use FigonacciPhpactorial\Client\HttpClient;
 use FigonacciPhpactorial\Client\LocalClient;
 
 include __DIR__ . '/vendor/autoload.php';
 
-$local = new LocalClient(getenv('FIBFAC_MOD'));
-$httpPhp = new HttpClient('http://nginx-http');
-$httpGo = new HttpClient('http://go-http');
-$grpcGo = new GrpcClient('go-grpc:80');
+$clients = [
+    'local' => new LocalClient(getenv('FIBFAC_MOD')),
+    'httpPhp' => new HttpClient('http://nginx-http'),
+    'httpGo' => new HttpClient('http://go-http'),
+    'grpcGo' => new GrpcClient('go-grpc:80'),
+];
 
-echo "Local HelloWorld: '{$local->helloWorld()}'\n";
-echo "HTTP PHP HelloWorld: '{$httpPhp->helloWorld()}'\n";
-echo "HTTP GO HelloWorld: '{$httpGo->helloWorld()}'\n";
-echo "GRPC GO HelloWorld: '{$grpcGo->helloWorld()}'\n";
-echo "\n-------------\n\n";
-
-echo "Local FibFac: '{$local->fibFac(10)}'\n";
-echo "HTTP PHP FibFac: '{$httpPhp->fibFac(10)}'\n";
-echo "HTTP GO FibFac: '{$httpGo->fibFac(10)}'\n";
-echo "GRPC GO FibFac: '{$grpcGo->fibFac(10)}'\n";
+array_walk(
+    $clients,
+    function (ClientInterface $c, string $name) {
+        $t0 = microtime(true);
+        $hello = $c->helloWorld();
+        $t1 = microtime(true);
+        $fibFac = $c->fibFac(10);
+        $t2 = microtime(true);
+        echo "$name - HelloWorld: '$hello' ... \t" . ($t1-$t0) . "s\n";
+        echo "$name - FibFac: '$fibFac' ... \t" . ($t2-$t1) . "s\n";
+    }
+);
