e46fd329e45441c3036a044a65db3e3555f97250
David Álvarez Pons <david_alvarezpons@hotmail.com>
2020-05-09 12:13:02 +0200

    Add fibFacPhp call to benchmark

--- php/benchmark.php
+++ php/benchmark.php
@@ -8,7 +8,7 @@ use FigonacciPhpactorial\Client\LocalClient;
 include __DIR__ . '/vendor/autoload.php';
 
 $clients = [
-    'local' => new LocalClient(getenv('FIBFAC_MOD')),
+    'localPhp' => new LocalClient(getenv('FIBFAC_MOD')),
     'httpPhp' => new HttpClient('http://nginx-http'),
     'httpGo' => new HttpClient('http://go-http'),
     'grpcGo' => new GrpcClient('go-grpc:80'),
@@ -20,6 +20,14 @@ array_walk(
         $t0 = microtime(true);
         $fibFac = $c->fibFac(10);
         $t1 = microtime(true);
-        echo "$name - FibFac: '$fibFac' ... \t" . ($t1-$t0) . "s\n";
+        $fibFacPhp = $c->fibFacPhp(10);
+        $t2 = microtime(true);
+        if ($fibFac === $fibFacPhp) {
+            echo "$name - FibFac: '$fibFac' ... \t\t" . ($t1-$t0) . "s\n";
+            echo "$name - FibFacPhp: '$fibFacPhp' ... \t" . ($t2-$t1) . "s\n";
+        } else {
+            echo "$name - Not maching results: '$fibFac' vs. '$fibFacPhp'\n";
+        }
+        echo "\n";
     }
 );
