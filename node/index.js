b0c97d982b95bc74c2e1dfc28ed945ce1e674ff7
David Álvarez Pons <david_alvarezpons@hotmail.com>
2020-05-16 16:40:44 +0200

    Add -php endpoints to node service

--- node/index.js
+++ node/index.js
@@ -1,3 +1,4 @@
+var exec = require('child_process');
 var express = require('express');
 var fibFac = require('./calculator/fibFac')
 var textLen = require('./calculator/textLen')
@@ -13,9 +14,37 @@ app.get('/fibfac', function (req, res) {
     res.send("" + fibFac(req.query.a, process.env.FIBFAC_MOD));
 });
 
+app.get('/fibfac-php', function (req, res) {
+    console.log("Hande GET on route: " + req.path)
+    exec.exec(
+        "php /php-code/scripts/fibfac.php " + req.query.a,
+        function (error, out) {
+            if (error) {
+                res.status(500)
+                res.send(error)
+            }
+            res.send(out);
+        }
+    );
+});
+
 app.get('/textlen', function (req, res) {
     console.log("Hande GET on route: " + req.path)
     res.send(textLen(req.query.a));
+});
+
+app.get('/textlen-php', function (req, res) {
+    console.log("Hande GET on route: " + req.path)
+    exec.exec(
+        "php /php-code/scripts/textlen.php " + req.query.a,
+        function (error, out) {
+            if (error) {
+                res.status(500)
+                res.send(error)
+            }
+            res.send(out);
+        }
+    );
 });
 
 app.listen(80, function () {
