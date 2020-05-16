b09a397557743f46a95f6ebd5ef6be22d3bc278c
David Álvarez Pons <david_alvarezpons@hotmail.com>
2020-05-16 16:10:50 +0200

    Add log for routing

--- node/index.js
+++ node/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
+    console.log("Hande GET on route: " + req.path)
     res.send('Hello World!');
 });
 
